f2b7313f91bf1dbd5274da4b8d0285675b554768
Daniel Bergstein <db102010@aol.com>
2018-06-26 13:53:25 +0300

    Beggning to add functionallity for the server

---
+++ tornadoServer.py
@@ -0,0 +1,15 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        self.write('<html><body><form method="POST">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.write('<input type="button" value="New Submission" onClick="window.location.reload()">')
+        airportName = airportDB3.posts.find_one({'name': self.get_body_argument("message")})
+        self.write("Phone: " + airportName['phone'])
