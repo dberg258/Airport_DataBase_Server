e13b69d3e17e3146bcc6a942e8e39e7361cf5b16
Daniel Bergstein <db102010@aol.com>
2018-06-26 13:16:00 +0300

    Starting to add some of mongoDB functionallity.

--- airportDB.py
+++ airportDB.py
@@ -1,3 +1,8 @@
+
+
+import requests
+
+
 def fileParser(fileName):
     file = open(fileName)
     locations = []
@@ -8,3 +13,16 @@ def fileParser(fileName):
         locations.append(line)
     # print(locations)
     return locations
+
+
+
+headers = {
+        'X-API-Key': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVkZW50aWFsX2lkIjoiY3JlZGVudGlh'
+                     'bHxOMHFkRzVTd1hxM2VrYzJrblFFN0g1UEdZa0ciLCJhcHBsaWNhdGlvbl9pZCI6ImFwcGxpY'
+                     '2F0aW9ufE5sbDY3RE1GdkJYcHk3aUtMOVgwWVR4RUI0NEsiLCJvcmdhbml6YXRpb25faWQiOi'
+                     'JkZXZlbG9wZXJ8NzNrUFgzZXUweXdxWUxGOUxET1hwaWtnbm5iWiIsImlhdCI6MTUyOTgzODE'
+                     '1NH0.f43-dAkFA6VXdqoueqTUe6n9w1-TWDw3zyVJEZ86GxM'
+    }
+
+
+r = requests.get('https://api.airmap.com/status/v2/point', params=query, headers=headers)
