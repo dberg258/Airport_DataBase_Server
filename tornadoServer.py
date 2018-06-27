9fa42d0efaf101cb101402ea958bee1626b205fb
Daniel Bergstein <db102010@aol.com>
2018-06-27 15:28:14 +0300

    Added spellcheck functionallity

--- tornadoServer.py
+++ tornadoServer.py
@@ -1,12 +1,30 @@
 from pymongo import MongoClient
 import tornado.ioloop
 import tornado.web
-import time
 
 
 client = MongoClient()
 db = client.Airports
 posts = db.posts
+
+
+def spellCheck(inputted):
+    airportNames = []
+    for post in posts.find():
+        airportNames.append(post['name'])
+
+    recommendations = set()
+
+    for word in airportNames:
+        letterCount = 0
+        for letter in word:
+            if inputted.__contains__(letter):
+                letterCount += 1
+
+        if letterCount / len(word) >= .65:
+            recommendations.add(word)
+
+    return recommendations
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -24,10 +42,13 @@ class MainHandler(tornado.web.RequestHandler):
                        + '</textarea><br><input type="button" value="New Submission" '
                          'style="background: transparent;" onClick="window.location.reload()"></div>')
         except:
-            self.write('Not A Valid Airport!')
+
+            spellCheckRecs = spellCheck(self.get_body_argument("message"))
+            self.write('Not A Valid Airport</br></br>')
+            self.write("Did you mean:</br></br>")
+            for word in spellCheckRecs:
+                self.write(word+"<br>")
             self.render('mainPage.html')
-
-
 
 
 def make_app():
